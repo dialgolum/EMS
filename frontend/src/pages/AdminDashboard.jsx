90cc0157c01466374776f8caf31ed65b9eddb869
dilumhapugoda <dilumhapugoda@gmail.com>
2025-01-03 13:38:10 +0530

    till admin

---
+++ frontend/src/pages/AdminDashboard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useAuth } from '../context/authContext'
+import { useNavigate } from 'react-router-dom'
+
+const AdminDashboard = () => {
+  const {user, loading} = useAuth()
+  const navigate = useNavigate()
+
+  if(loading) {
+    return <div>Loading....</div>
+  }
+
+  if(!user) {
+    navigate('/login')
+  }
+
+  return (
+    <div>
+      Admin Dashboard {user.name}
+    </div>
+  )
+}
+
+export default AdminDashboard
